5a480073f64018fcfaa5e4db037f0b319a8ab7ca
Stalker2106 <stalker2106x@gmail.com>
2024-12-08 02:46:29 +0100

    reorganized assets, added timeline

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const nunjucks = require('nunjucks')
 const fs = require('fs')
 
-const contentPath = './content'
-const outputPath = './dist'
-const templatesPath = './templates'
+const contentPath = 'content'
+const outputPath = 'dist'
+const templatesPath = 'templates'
 
 async function main() {
     nunjucks.configure({ autoescape: true })
-    const pages = ['index']
+    const pages = ['index', 'projects']
     if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath)
     // Iterate on all pages, render them and export as HTML in outputPath
     for (const page of pages) {
@@ -19,9 +19,15 @@ async function main() {
             const posts = JSON.parse(fs.readFileSync(`${contentPath}/posts.json`, 'utf8'))
             contentData.posts = []
             for (const postData of posts) {
-                postData.content = fs.readFileSync(`${contentPath}/posts_content/${postData.contentid}.html`, 'utf8')
-                contentData.posts.push(postData)
+                const postContentPath = `${contentPath}/posts_content/${postData.contentid}`
+                const postContent = nunjucks.render(`${postContentPath}/index.html`, { contentPath: `../${postContentPath}` })
+                contentData.posts.push({
+                    ...postData,
+                    content: postContent,
+                })
             }
+        } else if (page === 'projects') {
+            contentData.projects = JSON.parse(fs.readFileSync(`${contentPath}/projects.json`, 'utf8'))
         }
         pageContent = nunjucks.render(`${templatesPath}/pages/${page}.html`, contentData)
         const pageData = {
