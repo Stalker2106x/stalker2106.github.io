76c16ed959ab70f1f7c795452931687288999698
Stalker2106x <stalker2106x@gmail.com>
2024-12-08 17:01:43 +0100

    Feat/improvements (#1)

---
+++ config.js
@@ -0,0 +1,12 @@
+const ContentPath = 'content'
+const OutputPath = 'dist'
+const TemplatesPath = 'templates'
+const PostsContentPath =`${OutputPath}/posts_content`
+
+module.exports = {
+  PostsPerPage: 10,
+  ContentPath,
+  OutputPath,
+  TemplatesPath,
+  PostsContentPath
+}
--- index.js
+++ index.js
@@ -1,41 +1,75 @@
 const nunjucks = require('nunjucks')
+const path = require('path')
 const fs = require('fs')
+const { getTagList } = require('./utils')
 
-const contentPath = 'content'
-const outputPath = 'dist'
-const templatesPath = 'templates'
+const {
+    PostsPerPage,
+    PostsContentPath,
+    TemplatesPath,
+    ContentPath,
+    OutputPath
+} = require('./config')
+
+
+function injectPostsContent(posts) {
+    const filledPosts = []
+    for (const postData of posts) {
+        const postContentPath = `${PostsContentPath}/${postData.contentid}`
+        const postContent = nunjucks.render(`${postContentPath}/index.html`, { contentPath: `../${postContentPath}` })
+        filledPosts.push({
+            ...postData,
+            content: postContent
+        })
+    }
+    return filledPosts
+}
+
+function generatePage(pageName, outputName, pageData) {
+    const navigation = nunjucks.render(`${TemplatesPath}/navigation.html`, { pageName })
+    let pageContent = null
+    pageContent = nunjucks.render(`${TemplatesPath}/pages/${pageName}.html`, pageData)
+    const fullpageData = {
+        navigation,
+        pageContent
+    }
+    const fullpageHtml = nunjucks.render(`${TemplatesPath}/main.html`, fullpageData)
+    fs.writeFileSync(`${OutputPath}/${outputName}.html`, fullpageHtml)
+}
+
+function cleanDist() {
+    const dir = fs.opendirSync(OutputPath)
+    let dirent
+    while ((dirent = dir.readSync()) !== null) {
+        if (dirent.isFile() && dirent.name.endsWith('.html')) {
+            fs.unlinkSync(path.join(OutputPath, dirent.name))
+        }
+    }
+    dir.closeSync()
+}
 
 async function main() {
-    nunjucks.configure({ autoescape: true })
-    const pages = ['index', 'projects']
-    if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath)
-    // Iterate on all pages, render them and export as HTML in outputPath
-    for (const page of pages) {
-        const navigation = nunjucks.render(`${templatesPath}/navigation.html`, { page })
-        let pageContent = null
-        // Render page content
-        let contentData = {}
-        if (page === 'index') {
-            const posts = JSON.parse(fs.readFileSync(`${contentPath}/posts.json`, 'utf8'))
-            contentData.posts = []
-            for (const postData of posts) {
-                const postContentPath = `${contentPath}/posts_content/${postData.contentid}`
-                const postContent = nunjucks.render(`${postContentPath}/index.html`, { contentPath: `../${postContentPath}` })
-                contentData.posts.push({
-                    ...postData,
-                    content: postContent,
-                })
-            }
-        } else if (page === 'projects') {
-            contentData.projects = JSON.parse(fs.readFileSync(`${contentPath}/projects.json`, 'utf8'))
+    const posts = JSON.parse(fs.readFileSync(`${ContentPath}/posts.json`, 'utf8'))
+    const projects = JSON.parse(fs.readFileSync(`${ContentPath}/projects.json`, 'utf8'))
+    const tags = getTagList(posts)
+    try {
+        nunjucks.configure({ autoescape: true })
+        cleanDist()
+        // Generate posts pages
+        const pageCount = Math.floor(posts.length / PostsPerPage) + (posts % PostsPerPage == 0 ? 0 : 1)
+        for (let pageNumber = 0; pageNumber < pageCount; pageNumber++) {
+            const pagePosts = posts.slice(pageNumber * PostsPerPage, (pageNumber + 1) * PostsPerPage)
+            generatePage('index', pageNumber == 0 ? 'index' : `index-${pageNumber}`, { pageNumber, pageCount, posts: injectPostsContent(pagePosts), tags })
         }
-        pageContent = nunjucks.render(`${templatesPath}/pages/${page}.html`, contentData)
-        const pageData = {
-            navigation,
-            pageContent
+        // Generate projects page
+        generatePage('projects', 'projects', { projects })
+        // Generate tags pages
+        for (const tag of tags) {
+            const pagePosts = posts.filter((p) => p.tags.includes(tag.name))
+            generatePage('tag', `tag-${tag.name}`, { posts: injectPostsContent(pagePosts), tagName: tag.name })
         }
-        const pageHtml = nunjucks.render(`${templatesPath}/main.html`, pageData)
-        fs.writeFileSync(`${outputPath}/${page}.html`, pageHtml)
+    } catch (e) {
+        console.error(e)
     }
 }
 main()
